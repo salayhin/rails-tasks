5e9270f9b19d4a663e0eec6ce4d18b38423eabfd
Sirajus Salayhin <salayhin@gmail.com>
2012-10-20 05:28:40 +0600

    Tasks project added

---
+++ app/models/task.rb
@@ -0,0 +1,5 @@
+class Task < ActiveRecord::Base
+  attr_accessible :task
+
+  validates :task, presence: true
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tasks::Application.config.secret_token = 'deae597d12d838e9b53b45bc94fbf92c586280d1634b14ee15a090f9953800d6e117f0e975221d5a2d15ac16fc60a04e86c524f749fc24333d35176f6a8a1a86'
